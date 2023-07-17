dd953f2e3e60d0cafe37168580ca21d08bdc34df
DafeiQiu123 <138055520+DafeiQiu123@users.noreply.github.com>
2023-07-17 16:39:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template('testing.html')
+
+@app.route('/preview_document')
+def preview_document():
+    pdf_path = 'static/itin.pdf'
+    return send_file(pdf_path, mimetype='application/pdf')
+
+@app.route('/download_document')
+def download_document():
+    docx_path = 'static/itin.docx'
+    return send_file(docx_path, as_attachment=True)
 if __name__ == "__main__":
-    app.run('127.0.0.1', 80, debug=True)
+    app.run()
 
 
